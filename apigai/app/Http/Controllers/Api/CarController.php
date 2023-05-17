5963d680c74be214ed01441ca39ac211800ddf2b
Ilya Shevchenko <Sevc_ph43@student.itstep.org>
2023-05-17 23:29:20 +0300

    add new method and fix cors

--- apigai/app/Http/Controllers/Api/CarController.php
+++ apigai/app/Http/Controllers/Api/CarController.php
@@ -10,17 +10,23 @@ class CarController extends Controller
 {
     public function index(Request $request){
         if(strlen($request['query']) == 8){
-            $client = new \GuzzleHttp\Client(['headers' => ['Accept' => 'application/json','X-Api-Key'=> 'a4ac865acdf651589a1fb19439037241']]);
+            $client = new \GuzzleHttp\Client(['headers' => ['Accept' => 'application/json','X-Api-Key'=> 'a4ac865acdf651589a1fb19439037241','Access-Control-Allow-Origin' => '*']]);
             $gai =  $client->request('GET', 'https://baza-gai.com.ua/nomer/'.$request['query']);
             $RESULT =  $gai->getBody();
             echo $RESULT;
         }
         elseif (strlen($request['query']) == 17){
-            $client = new \GuzzleHttp\Client(['headers' => ['Accept' => 'application/json','X-Api-Key'=> 'a4ac865acdf651589a1fb19439037241']]);
+            $client = new \GuzzleHttp\Client(['headers' => ['Accept' => 'application/json','X-Api-Key'=> 'a4ac865acdf651589a1fb19439037241','Access-Control-Allow-Origin' => '*']]);
             $gai =  $client->request('GET', 'https://baza-gai.com.ua/vin/'.$request['query']);
             $RESULT =  $gai->getBody();
             echo $RESULT;
         }
         return $request;
+    }
+    public  function getModel(Request $request){
+        $client = new \GuzzleHttp\Client(['headers' => ['Accept' => 'application/json','X-Api-Key'=> 'a4ac865acdf651589a1fb19439037241','Access-Control-Allow-Origin' => '*']]);
+        $gai =  $client->request('GET', 'https://baza-gai.com.ua/make/'.$request['slug']);
+        $RESULT =  $gai->getBody();
+        echo  $RESULT;
     }
 }
--- apigai/routes/api.php
+++ apigai/routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/car',[\App\Http\Controllers\Api\CarController::class, 'index']);
+Route::get('/mark',[\App\Http\Controllers\Api\CarController::class, 'getModel']);
